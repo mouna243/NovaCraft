874c02f73907009e172d399c6bd3f06d091503f5
mouna243 <mounasaoui2108@gmail.com>
2025-12-07 12:32:13 +0100

    just organisation de commentaire

--- views/home.php
+++ views/home.php
@@ -6,7 +6,6 @@
     <div class="">
    <h1 class="text-[50px] text-[#16b7d9]"><strong>DigitalWave</strong></h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-    </div>
- 
+    </div> 
 <img src="../assetes/home.jpeg" alt="hero" class="absolut rounded ">
 </div>
